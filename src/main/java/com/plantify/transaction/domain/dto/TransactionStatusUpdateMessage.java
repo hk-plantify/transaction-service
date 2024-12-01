4fd37db588487884dee5c7c6b1d20481bbe52349
zzoo0123 <0123suh@gmail.com>
2024-12-02 02:19:46 +0900

    feat: entity dto 추가 및 수정

--- src/main/java/com/plantify/transaction/domain/dto/TransactionStatusUpdateMessage.java
+++
@@ -1,8 +0,0 @@
-package com.plantify.transaction.domain.dto;
-
-import com.plantify.transaction.domain.entity.Status;
-
-public record TransactionStatusUpdateMessage(
-        Long transactionId,
-        Status status
-) {}
