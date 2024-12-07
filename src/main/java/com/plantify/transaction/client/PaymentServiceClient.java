00316ebedf4b39b43a6a1094d8aaf30d6fd306a2
zzoo0123 <0123suh@gmail.com>
2024-12-07 16:19:14 +0900

    feat: paymentServiceClient 수정

--- src/main/java/com/plantify/transaction/client/PaymentServiceClient.java
+++ src/main/java/com/plantify/transaction/client/PaymentServiceClient.java
@@ -1,7 +1,7 @@
 package com.plantify.transaction.client;
 
+import com.plantify.transaction.domain.dto.PaymentRequest;
 import com.plantify.transaction.domain.dto.PaymentResponse;
-import com.plantify.transaction.domain.dto.TransactionRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "payment-service", url = "${payment.service.url}")
 public interface PaymentServiceClient {
 
-    @PostMapping("/payments/process")
-    PaymentResponse processPayment(@RequestBody TransactionRequest request);
+    @PostMapping("/v1/payments/process")
+    PaymentResponse processPayment(@RequestBody PaymentRequest request);
 }
