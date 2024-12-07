80b3fe6dc2e3d0ade5bb0335f95363e6936b01c2
zzoo0123 <0123suh@gmail.com>
2024-12-07 16:18:49 +0900

    feat: global exception, response 수정

--- src/main/java/com/plantify/transaction/global/exception/ApplicationException.java
+++ src/main/java/com/plantify/transaction/global/exception/ApplicationException.java
@@ -1,18 +1,22 @@
 package com.plantify.transaction.global.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 public class ApplicationException extends RuntimeException {
 
+    @Getter
     private final HttpStatus httpStatus;
     private final String message;
 
     public ApplicationException(ErrorCode errorCode) {
-        httpStatus = errorCode.getHttpStatus();
-        message = errorCode.getMessage();
+        super(errorCode.getMessage());
+        this.httpStatus = errorCode.getHttpStatus();
+        this.message = errorCode.getMessage();
     }
 
-    public HttpStatus getHttpStatus() {
-        return null;
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/com/plantify/transaction/global/response/ApiResponse.java
+++ src/main/java/com/plantify/transaction/global/response/ApiResponse.java
@@ -7,19 +7,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public class ApiResponse<T> {
-    private HttpStatus status;
+
+    private Integer status;
     private String message;
     private T data;
 
     public static <T> ApiResponse<T> ok() {
-        return new ApiResponse<>(HttpStatus.OK, "성공", null);
+        return new ApiResponse<>(HttpStatus.OK.value(), "성공", null);
     }
 
     public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<>(HttpStatus.OK, "성공", data);
+        return new ApiResponse<>(HttpStatus.OK.value(), "성공", data);
     }
 
     public static <T> ApiResponse<T> fail(HttpStatus status, String message) {
-        return new ApiResponse<>(status, message, null);
+        return new ApiResponse<>(status.value(), message, null);
     }
 }
